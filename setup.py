769716d74eb6e17fed085ed918baa07d9ff59a65
Martin Jespersen <majespersen@deloitte.dk>
2021-03-06 22:47:30 +0100

    small fixes

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ requirements = [
     "danlp",
     "gensim==3.8.1",
     "pip==19.2.3",
-    "click-7.1.2",
-    "smart-open-3.0.0",
+    "click==7.1.2",
+    "smart-open==3.0.0",
     "pytest==4.6.5",
     "pytest-runner==5.1",
 ]
