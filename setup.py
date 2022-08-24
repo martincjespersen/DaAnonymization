515db6c30291efbc8f9cef4b5e96da55c62c055c
Martin Jespersen <martincjespersen@gmail.com>
2022-08-24 22:38:06 +0200

    fixed dacy version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "dacy[large]==1.2.3",
+    "dacy",
 ]
 
 setup_requirements = [
@@ -25,17 +25,17 @@ test_requirements = [
 setup(
     author="Martin Closter Jespersen",
     author_email="martincjespersen@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="A simple pipeline wrapped around SpaCy and DaCy for anonymizing danish corpora.",
     entry_points={
